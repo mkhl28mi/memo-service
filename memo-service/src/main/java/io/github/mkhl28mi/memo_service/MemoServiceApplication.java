22a44411b50970aa5463fba51219454243cb3910
mkhl28mi <mostipan.mykhailo@gmail.com>
2026-06-30 11:06:11 +0300

    Initial commit!

---
+++ memo-service/src/main/java/io/github/mkhl28mi/memo_service/MemoServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.mkhl28mi.memo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoServiceApplication.class, args);
+	}
+
+}
---
+++ memo-service/src/test/java/io/github/mkhl28mi/memo_service/MemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mkhl28mi.memo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
